01d5678283dff081f4c8af0003763a635f0dc797
Azania2016 <Buzanimgamlele@gmail.com>
2016-11-17 10:50:56 +0200

    Save this file

---
+++ lunchWhere.js
@@ -0,0 +1,12 @@
+//in a file called lunchWhere.js
+var lunchWhere = function(bankBalance){
+    if (bankBalance > 4500 ){
+        return "Go and get KFC!"
+    }
+    else if (bankBalance < 4500 && bankBalance > 1500){
+        return "Go to PnP!"
+    }
+    else{
+        return "Two minute noodles today!";
+    }
+};
